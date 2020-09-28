f1ffa511706869eab127670d3f2ff7f7a8db6d8b
Victor De la Luz <itztli@gmail.com>
2020-09-28 11:58:34 -0500

    including first library

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "people.h"
 
 int main(){
-  printf("Hello World\n");
+  People victor;
+  victor = new_People(39.0,80.0,1.80);
+  print_People(victor);
+  //printf("Hello World\n");
   return 0;
 }
